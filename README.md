6f5dbfb15ff824f7d63a5d21f5bfef2691b793b8
George Stagas <gstagas@gmail.com>
2011-10-05 01:15:54 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,169 @@
+# simpl
+
+Simple sockets anywhere
+
+
+## Introduction
+
+simpl is a unified API for websockets and unix sockets. I wanted to send simple
+text messages around and have the same API, whether it's a web server or a shell
+app. This does just that.
+
+
+## Installation
+
+`npm install simpl`
+
+
+## Example
+
+```javascript
+var simpl = require('simpl')
+
+var server = simpl.createServer(8080)
+
+server.on('message', function (message, conn) {
+  console.log("Got message from client:", message)
+  server.close()
+})
+
+server.on('ready', function () {
+  console.log("Server listening")
+
+  var client = simpl.createClient(8080)
+
+  client.on('connect', function () {
+    client.send("Hello, world!")
+    client.close()
+  })
+})
+```
+
+
+## API
+
+
+
+
+### Server
+
+
+#### server = simpl.createServer(path)
+
+Creates a unix socket server at `path`.
+
+#### server = simpl.createServer(port[, host])
+
+Creates a webserver with a websocket server attached to it and listens on `port`
+and `host`.
+
+#### server = simpl.createServer(httpServer)
+
+Attaches a websocket server to an existing http server.
+
+
+
+### Server Methods
+
+
+#### server.broadcast(message)
+
+Broadcasts a message to all connected clients.
+
+#### server.close()
+
+Closes the server. In case where it's attached to a http server it closes the
+http server.
+
+
+
+### Server Events
+
+
+#### ready
+
+Emits when server is listening for connections.
+
+#### connection (connection)
+
+Emits when a client connects to the server. Callbacks a `Connection` object.
+
+#### message (message, connection)
+
+Emits when server receives a message.
+
+
+
+
+### Connection
+
+
+### Connection Methods
+
+
+#### conn.send(message)
+
+Sends a message.
+
+#### conn.broadcast(message)
+
+Broadcasts a message to all clients except this one.
+
+#### conn.close()
+
+Closes a connection.
+
+
+
+### Connection Events
+
+
+#### message (message)
+
+Emits when a message is received.
+
+#### close
+
+Emits when the connection closes.
+
+
+
+
+### Client
+
+
+#### client = simpl.createClient(path)
+
+Creates a client and connects to a unix socket server at `path`.
+
+#### client = simpl.createClient(port[, host])
+
+Creates a client and connects to a websocket server on `port` and `host`.
+
+
+
+### Client Methods
+
+
+#### client.send(message)
+
+Sends a message to the server.
+
+#### client.close()
+
+Closes the connection.
+
+
+
+### Client Events
+
+
+#### connect
+
+Emits when the client connects to the server.
+
+#### message (message)
+
+Emits when the client receives a message.
+
+
---
+++ test/test.js
@@ -0,0 +1,96 @@
+var simpl = require('../')
+var test = require('tap').test
+
+var tests = {
+  "websocket server standalone": { port: 8080 }
+, "websocket server on http server": { server: require('http').createServer() }
+, "unix socket server": { path: '/tmp/test-simpl-unix-socket-server.sock' }
+}
+
+Object.keys(tests).forEach(function (desc) {
+
+  test(desc, function (t) {
+
+    var server
+      , client, client2
+      , conn, conn2
+
+    var params = tests[desc]
+
+    t.test("create server", function (t) {
+      t.plan(1)
+      server = simpl.createServer(params)
+      server.on('ready', function () {
+        t.pass("server is ready")
+      })
+      if (params.server) {
+        params.server.listen(8081)
+        params.port = 8081
+      }
+    })
+
+    t.test("create client", function (t) {
+      t.plan(2)
+      client = simpl.createClient(params)
+      client.on('connect', function () {
+        t.pass("client connected")
+      })
+      server.once('connection', function (c) {
+        t.pass("server received connection")
+        conn = c
+      })
+    })
+
+    t.test("server sending 'hello' to client", function (t) {
+      t.plan(1)
+      client.once('message', function (message) {
+        t.equals(message, 'hello', "client received 'hello'")
+      })
+      conn.send('hello')
+    })
+
+    t.test("create client 2", function (t) {
+      t.plan(2)
+      client2 = simpl.createClient(params)
+      client2.on('connect', function () {
+        t.pass("client 2 connected")
+      })
+      server.once('connection', function (c) {
+        t.pass("server received connection 2")
+        conn2 = c
+      })
+    })
+
+    t.test("server broadcast 'hello' to clients", function (t) {
+      t.plan(2)
+      client.once('message', function (message) {
+        t.equals(message, 'hello', "client 1 received 'hello'")
+      })
+      client2.once('message', function (message) {
+        t.equals(message, 'hello', "client 2 received 'hello'")
+      })
+      server.broadcast('hello')
+    })
+
+    t.test("client sending 'hello' to server", function (t) {
+      t.plan(3)
+      server.once('message', function (message, c) {
+        t.equals(message, 'hello', "server received 'hello'")
+        t.equals(typeof c, 'object', "with a connection object")
+      })
+      conn.once('message', function (message) {
+        t.equals(message, 'hello', "server connection received 'hello'")
+      })
+      client.send('hello')
+    })
+ 
+    t.test("ending test " + desc, function (t) {
+      client.close()
+      client2.close()
+      server.close()
+      t.end()
+    })
+
+  })
+
+})
